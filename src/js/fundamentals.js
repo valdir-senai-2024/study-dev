ebd226c6b01e593d016ea0098283f1df28a89e2d
valdir-senai-2024 <valdir.p.nunes@aluno.senai.br>
2024-12-10 17:10:23 -0300

    added javascript fundamentals

---
+++ src/js/fundamentals.js
@@ -0,0 +1,22 @@
+// variables
+let variavel = "sou alteravel"
+const constante = "não é redeclarável"
+
+// console
+console.log(variavel)
+console.error(constante)
+console.info('sou uma informação')
+console.warn('var é depreciado')
+
+// tipo de dados
+const  lastName = 'correa'
+let age = 25
+const person = {
+    name:'joão',
+    age: 16,
+    gender: 'male'
+
+}
+const fruits = ['banana', 'uva']
+
+console.table(fruits)
